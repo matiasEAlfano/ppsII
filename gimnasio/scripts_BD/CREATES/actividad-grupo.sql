c937598634e0d393ecea785142792e6eb4ddb517
mneagoe <mauro_ssj@hotmail.com>
2015-06-13 17:05:52 -0300

    ..

--- gimnasio/scripts_BD/CREATES/actividad-grupo.sql
+++ gimnasio/scripts_BD/CREATES/actividad-grupo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 13-06-2015 a las 17:03:47
+-- Tiempo de generación: 06-06-2015 a las 22:53:24
 -- Versión del servidor: 5.5.41-0ubuntu0.14.04.1
 -- Versión de PHP: 5.5.9-1ubuntu4.9
 
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `actividad-grupo` (
   `id` int(11) NOT NULL,
   `idTipo` int(11) NOT NULL,
   `descripcion` varchar(25) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Volcado de datos para la tabla `actividad-grupo`
@@ -40,13 +40,17 @@ INSERT INTO `actividad-grupo` (`id`, `idTipo`, `descripcion`) VALUES
 (1, 1, 'Acuatica'),
 (7, 1, 'Aerobica'),
 (8, 1, 'Spinning'),
-(9, 2, 'Muscular'),
+(9, 2, 'Bodypump'),
 (10, 2, 'Muscular Combinado'),
 (11, 3, 'Pilates'),
 (12, 3, 'Elongacion'),
 (13, 3, 'Yoga'),
-(14, 4, 'Funcional'),
-(15, 5, 'Tecnicas Especificas');
+(14, 4, 'Megacross'),
+(15, 5, 'boxeo'),
+(16, 5, 'Danza Arabe'),
+(17, 5, 'Karate Do'),
+(18, 5, 'Salsa'),
+(19, 5, 'Taekwondo');
 
 --
 -- Índices para tablas volcadas
@@ -66,7 +70,7 @@ ALTER TABLE `actividad-grupo`
 -- AUTO_INCREMENT de la tabla `actividad-grupo`
 --
 ALTER TABLE `actividad-grupo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
 --
 -- Restricciones para tablas volcadas
 --
