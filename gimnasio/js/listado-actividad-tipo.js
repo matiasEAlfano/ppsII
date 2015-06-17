ec7558178977004154af1014ecfc637b00e8a95c
mneagoe <mauro_ssj@hotmail.com>
2015-06-17 09:25:30 -0300

    arregle el abm de actividad tipo incorporando nuevamente los archivos que habia eliminado

--- gimnasio/js/listado-actividad-tipo.js
+++ gimnasio/js/listado-actividad-tipo.js
@@ -16,7 +16,7 @@
                     var html = '<tr> \
                     <td class="acti-tipoDescripcion">'+tipo.descripcion+'</td> \
                     <td> \
-                        <form class="form-inline" action="actions/api-actividad-tipo.php" method="post"> \
+                        <form class="form-inline" action="actions/actions-actividad-tipo.php" method="post"> \
                             <input type="hidden" name="id" value="'+tipo.id+'"/> \
                             <button type="submit" name="action" value="editar" class="btn btn-success editar"><sapan class="glyphicon glyphicon-pencil" aria-hidden="true"></sapan></button> \
                             <button type="submit" name="action" value="eliminar" class="btn btn-danger eliminar"><sapan class="glyphicon glyphicon-remove" aria-hidden="true"></sapan></button> \
