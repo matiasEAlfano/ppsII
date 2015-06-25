f2538687560272b1adc74e00eb3e96e0ba016aa4
MATIAS ALFANO <matiasezequielalfano@gmail.com>
2015-06-25 12:31:01 -0300

    Borro toda la version actual para subir la de dropbox

--- gimnasio/partials/gestionHeader.php
+++
@@ -1,9 +0,0 @@
-<?php require("partials/header.php"); ?>
-        
-            
-            <h2 class="titulo-cuerpo">Gestion Administrador</h2>            
-            
-            <div class="container cuerpo-admin">
-<!--                ACA VA TODO EL CODIGO!!-->
-                                
-                <?php require("partials/menuGestiones.php"); ?>
--- gimnasio/scripts_BD/INSERTS/05232015_agregar_generos.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO generos(genero_nombre) VALUES ('Hombre');
-INSERT INTO generos(genero_nombre) VALUES ('Mujer');
-INSERT INTO generos(genero_nombre) VALUES ('Niños');
